aa487cf2e7366f142a7c2dd18f907ae8c1a5adaf
liutian <435757679@qq.com>
2024-09-30 22:05:59 +0800

    fix bug

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,4 +1,4 @@
-./usr/src/zkwasm-automata/target/release/csm_service --uri $URI & node /usr/src/zkwasm-automata/zkwasm-ts-server/node_modules/zkwasm-ts-server/src/service.js redis
+./usr/src/zkwasm-typescript-mini-server/target/release/csm_service --uri $URI & node /usr/src/zkwasm-automata/node_modules/zkwasm-ts-server/src/service.js redis
 
 # Wait for any process to exit
 wait -n
