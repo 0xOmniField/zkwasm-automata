678481507094b7a7eedd79d4fa6b5dceca05f90a
liutian <435757679@qq.com>
2024-10-01 00:27:16 +0800

    fix bug

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,9 +1,9 @@
 ./usr/src/zkwasm-typescript-mini-server/target/release/csm_service --uri $URI & node /usr/src/zkwasm-automata/ts/node_modules/zkwasm-ts-server/src/service.js redis
 
 # Wait for any process to exit
-wait -n
+# wait -n
 
-# Exit with status of the process that exited first
-exit $?
-~
+# # Exit with status of the process that exited first
+# exit $?
+# ~
 
