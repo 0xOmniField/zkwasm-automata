b5b0f77feefd08cd4a2cb0451e8ba8a6b8a6fe6e
liutian <435757679@qq.com>
2024-10-01 03:45:57 +0800

    fix

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,9 +1,2 @@
 node /usr/src/zkwasm-automata/ts/node_modules/zkwasm-ts-server/src/service.js redis
 
-# Wait for any process to exit
-wait -n
-
-# Exit with status of the process that exited first
-exit $?
-~
-
